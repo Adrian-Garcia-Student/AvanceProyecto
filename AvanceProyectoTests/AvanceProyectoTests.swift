d52bee6402d814aa32611863911699f4144298ae
maty <luis_e_alcantar@hotmail.com>
2025-08-27 16:31:03 -0600

    Initial Commit

---
+++ AvanceProyectoTests/AvanceProyectoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AvanceProyectoTests.swift
+//  AvanceProyectoTests
+//
+//  Created by Alumno on 27/08/25.
+//
+
+import Testing
+@testable import AvanceProyecto
+
+struct AvanceProyectoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
